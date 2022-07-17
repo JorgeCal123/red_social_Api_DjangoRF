c176225a44d7ebba77b6f4e08218a4338113a90e
JorgeCal123 <43484686+JorgeCal123@users.noreply.github.com>
2022-07-17 17:33:33 -0500

    Delete profiles_api directory
    delete

--- profiles_api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# importamos todos los modelos que creamos en el archivo models
-from profiles_api.models import *
-
-#Con esta linea le damos acceso al administrador para que puedea editar estos modelos desde la pagina admin
-admin.site.register(UserProfile)
-
